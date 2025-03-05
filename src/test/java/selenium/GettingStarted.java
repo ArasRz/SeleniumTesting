fcb008f53a9bcc70c510a218982a450145ab7a05
ArasRz <aras_164@hotmail.com>
2025-03-05 20:58:15 +0100

    First Selenium test

---
+++ src/test/java/selenium/GettingStarted.java
@@ -0,0 +1,26 @@
+package selenium;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.junit.Test;
+
+public class GettingStarted {
+    @Test
+    public void testGoogleSearch() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://duckduckgo.com/");
+
+        Thread.sleep(5000);  // Let the user actually see something!
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+
+        searchBox.sendKeys("ChromeDriver");
+        searchBox.submit();
+
+        Thread.sleep(5000);  // Let the user actually see something!
+
+        driver.quit();
+    }
+}
